8796394e77206277b0aa40afca261b1078d1a423
adarvin14 <amdarvin14@gmail.com>
2021-04-15 17:55:42 -0400

    form component

---
+++ client/src/components/Form.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import { addBlog } from '../actions';
+
+class Form extends Component {
+  state = {
+    artist: "",
+    album: "",
+    content: ""
+  }
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    this.props.addBlog(this.state, this.props.history);
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Create Review</h3>
+        <form onSubmit={ this.handleSubmit }>
+          <div>
+            <label htmlFor="artist">Artist </label>
+            <input type="text" id="artist" name="artist" value={ this.state.artist } onChange={ this.handleChange } />
+          </div>
+          <br />
+          <div>
+            <label htmlFor="album">Album </label>
+            <input type="text" id="album" name="album" value={ this.state.album } onChange={ this.handleChange } />
+          </div>
+          <br />
+          <div>
+            <p><label htmlFor="content">Content</label></p>
+            <textarea id="content" name="content" value={ this.state.content } onChange={ this.handleChange }></textarea>
+          </div>
+          <br />
+          <input type="submit" value="Create Review" />
+        </form>
+      </div>
+    )
+  }
+}
+
+export default connect(null, { addBlog })(Form)
