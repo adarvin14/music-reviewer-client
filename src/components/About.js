9b383ad0d518f71ea43e1df9791d34f2bb264033
adarvin14 <amdarvin14@gmail.com>
2021-04-19 15:51:31 -0400

    more troubleshooting

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class About extends Component {
+  render() {
+    return (
+      <div>
+        <h3>About this website</h3>
+
+        <p>Use this website to review your favorite or least favorite albums!</p>
+      </div>
+    )
+  }
+}
+
+export default About
