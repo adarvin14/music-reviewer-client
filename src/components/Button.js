9553963159d3059701b616ef5fafe160e4987350
adarvin14 <amdarvin14@gmail.com>
2021-04-26 18:04:30 -0400

    assessment

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,29 +3,37 @@ import React, { Component } from 'react'
 class Button extends Component {
 
     state = {
-        count: 0,
-        text: "",
+        count: 0
     }
 
     increaseCount = () => {
+        let newCount = this.state.count + 1
         this.setState({
-            count: this.state.count + this.state.text.length
+            count: newCount
         })
     }
+
+
 
     render() {
         return (
         <div>
             {/* keep track of the number of characters in the text box */}
             {/* update state whenever text is changed so that the button adds the correct # of char */}
-            <input type={this.text} onChange={e => this.setState({ text: e.target.value })} />
+                <input type="text" onClick={this.increaseCount} />
 
-            <button type="submit" onClick= {this.increaseCount}> Count!: {this.state.count} </button>
-            {/* use this.state.count so that the button keeps the current count */}
-        
+                <button type="submit" onClick= {this.increaseCount}> Count!: {this.state.count} </button>
         </div>
         )
     }
 }
+
+
+
+// 1. React Only
+// 2. It will need to be it’s own class component
+// 3. You can render it anywhere
+// 4. You can google, but you cannot google how to make an incrementor button in react.
+// 5 .You can google syntax but no direct walkthroughs
 
 export default Button
