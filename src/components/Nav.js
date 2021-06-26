7baf4c8ed25da7fba2140dfd75ba36a0a81b9035
adarvin14 <amdarvin14@gmail.com>
2021-06-26 14:54:44 -0400

    begin text repositioning and reformatting

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,16 +4,22 @@ import { Link } from 'react-router-dom';
 class Nav extends Component {
   render() {
     return (
-      <div>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/albums">Albums</Link></li>
-          <li><Link to="/albums/new">Create An Album</Link></li>
-          <li><Link to="/reviews">Reviews</Link></li>
-          <li><Link to="/reviews/new">Create A Review</Link></li>
-        </ul>
-      </div>
+      <header>
+        <div className="logo">
+          LOGO
+        </div>
+
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/albums">Albums</Link></li>
+            <li><Link to="/albums/new">Create An Album</Link></li>
+            <li><Link to="/reviews">Reviews</Link></li>
+            <li><Link to="/reviews/new">Create A Review</Link></li>
+          </ul>
+        </nav>
+      </header>
     )
   }
 }
